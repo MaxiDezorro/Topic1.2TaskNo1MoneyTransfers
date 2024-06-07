132653e71b3b25613b3a0e4a5e2a8a5203869fbb
Max M <maxidezorro@gmail.com>
2024-06-07 10:20:54 +0300

    add new gitignor, did main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 package ru.netology
 
+import kotlin.time.times
+
 fun main() {
+    val minCommission = 35
+    val commission = 0.075 // 0.75 процента
+    val amount: Int = 12348 // сумма перевода
+    val cashCommission = if (amount * commission < minCommission) minCommission
+    else (amount * commission).toInt()
+
+    println("Ваша комиссия за перевод составит $cashCommission рублей")
 
 }
